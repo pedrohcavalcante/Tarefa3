d485ee6b4eb6ec037f394cfb7f4fcebe69b3733a
Pedro H. B. Cavalcante <pedrohbcavalcante@ufrn.edu.br>
2018-03-19 10:31:18 -0300

    Início da definição do layout
    Início da definição da tela da questão 1

---
+++ app/src/main/java/com/example/pedrohenrique/tarefa3/Questao1.java
@@ -0,0 +1,13 @@
+package com.example.pedrohenrique.tarefa3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Questao1 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questao1);
+    }
+}
