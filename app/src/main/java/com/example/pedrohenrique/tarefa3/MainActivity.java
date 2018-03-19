0536c8851ad3ae6e9bccac23039b13db1a647b7c
Pedro H. B. Cavalcante <pedrinho.sjs@ufrn.edu.br>
2018-03-19 09:27:08 -0300

    Implementação tela inicial
    Feita a criação dos botões da tela inicial, de acordo com a tarefa solicitada.

--- app/src/main/java/com/example/pedrohenrique/tarefa3/MainActivity.java
+++ app/src/main/java/com/example/pedrohenrique/tarefa3/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.pedrohenrique.tarefa3;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void funcQuestao1(View view) {
+
+    }
+
+    public void funcQuestao2(View view){
+
     }
 }
