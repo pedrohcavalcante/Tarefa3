b3af25a2b1843da04c74894e6f4861900922c3f2
Pedro H. B. Cavalcante <pedrohbcavalcante@ufrn.edu.br>
2018-03-19 11:23:33 -0300

    finalizaçaõ do layout
    Layout finalizado. Falta back-end

--- app/src/main/java/com/example/pedrohenrique/tarefa3/Questao1.java
+++ app/src/main/java/com/example/pedrohenrique/tarefa3/Questao1.java
@@ -2,12 +2,58 @@ package com.example.pedrohenrique.tarefa3;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Questao1 extends Activity {
+
+    private EditText nome;
+    private RadioGroup grp_radio;
+    private RadioButton radio_sexo;
+    private TextView nome_view;
+    private TextView sexo_view;
+    private TextView nome_str;
+    private TextView sexo_str;
+    private String valor_sexo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questao1);
+
+        nome = findViewById(R.id.input_nome);
+        grp_radio = findViewById(R.id.group_sexo);
+        nome_view = findViewById(R.id.nome_str_view);
+        sexo_view = findViewById(R.id.sexo_str_view);
+    }
+
+    public void exibir(View view){
+        
+    }
+
+    public void limpar(View view){
+
+    }
+
+    public void onRadioButtonClicked(View view){
+        boolean checar = ((RadioButton) view).isChecked();
+
+        switch (view.getId()){
+            case R.id.rdo_masculino:
+                if (checar){
+                    valor_sexo = "Masculino";
+                }
+                break;
+            case R.id.rdo_feminino:
+                if (checar){
+                    valor_sexo = "Feminino";
+                }
+                break;
+        }
     }
 }
